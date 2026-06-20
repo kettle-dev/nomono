3e8466c43d3438a009cff564da05537dcd911fb1
Peter H. Boling <peter.boling@gmail.com>
2026-06-20 07:53:20 -0600

    🎨 Template bootstrap by kettle-jem v7.0.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,10 @@
 #       To generate coverage data KETTLE_RB_DEV=true must be set.
 begin
   require "kettle-soup-cover"
+  if Kettle::Soup::Cover::DO_COV
+    require "simplecov"
+    SimpleCov.start
+  end
   require "simplecov" if Kettle::Soup::Cover::DO_COV # `.simplecov` is run here!
 rescue LoadError => error
   # check the error message and re-raise when unexpected
