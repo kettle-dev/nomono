70af92091c722e79f42f4d018b8b8be5de9565cc
Peter H. Boling <peter.boling@gmail.com>
2026-06-29 10:00:57 -0600

    🎨 Template bootstrap by kettle-jem v7.0.0

--- lib/nomono.rb
+++ lib/nomono.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 # This gem
+require "version_gem"
+
 require_relative "nomono/version"
 require_relative "nomono/core"
+
+Nomono::Version.class_eval do
+  extend VersionGem::Basic
+end
