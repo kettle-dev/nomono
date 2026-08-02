0510e0793ea7ed5eb446487e3dc9025fd9f655b6
Peter H. Boling <peter.boling@gmail.com>
2026-08-01 20:58:18 -0600

    🔖 Bump gem version

--- lib/nomono/version.rb
+++ lib/nomono/version.rb
@@ -4,7 +4,7 @@ module Nomono
   # Version namespace for this gem.
   module Version
     # Current gem version.
-    VERSION = "1.1.2"
+    VERSION = "1.1.3"
   end
   # Current gem version exposed at the traditional constant location.
   VERSION = Version::VERSION # Traditional Constant Location
